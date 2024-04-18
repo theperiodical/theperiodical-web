ec5e2c1b77d80495d8a432fc0edb4a7774728a90
Subin S K <subinsk284@gmail.com>
2024-04-19 04:13:20 +0530

    fix: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export const Navbar=()=>{
-
-    return(
-<div className="flex flex-row items-center justify-between px-14 py-6 w-full">
-    <div className="">logo</div>
-    <div className="flex flex-row gap-4 items-center">
-        <Link href={'#home'} className="text-center">Home</Link>
-        <Link href={'#gists'} className="text-center">Gists</Link>
-        <Link href={'#about-us'} className="text-center">About Us</Link>
-        <Link href={'#contact-us'} className="text-center">Contact Us</Link>
-    <button className="w-28 h-10 bg-primary-dark text-white hover:bg-primary-hover">Subscribe</button></div>
-</div>
-    )
-}
+export const Navbar = () => {
+  return (
+    <div className="flex flex-row items-center justify-between px-14 py-6 w-full">
+      <div className="">logo</div>
+      <div className="flex flex-row gap-4 items-center">
+        <Link href={"/#home"} className="text-center">
+          Home
+        </Link>
+        <Link href={"/#gists"} className="text-center">
+          Gists
+        </Link>
+        <Link href={"/#about-us"} className="text-center">
+          About Us
+        </Link>
+        <Link href={"/#contact-us"} className="text-center">
+          Contact Us
+        </Link>
+        <button className="w-28 h-10 bg-primary-dark text-white hover:bg-primary-hover">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
+};
